bcfe593c14191d786359f5cbfcf74c89ab6359f4
Thanh Tran <76641137+huuthanh21@users.noreply.github.com>
2024-07-13 17:38:01 +0700

    Add transaction sending functionality to SendPage (#9)

---
+++ utils/transactionApi.ts
@@ -0,0 +1,26 @@
+import { API_ENDPOINT } from "@/utils/constants";
+
+async function sendTransaction(
+	sender: string,
+	recipient: string,
+	amount: number,
+	privateKey: string
+) {
+	const response = await fetch(`${API_ENDPOINT}/transaction/send`, {
+		body: JSON.stringify({ amount, privateKey, recipient, sender }),
+		headers: {
+			"Content-Type": "application/json",
+		},
+		method: "POST",
+	});
+	const data = await response.json();
+	return data as {
+		amount: number;
+		id: number;
+		recipient: string;
+		sender: string;
+		timestamp: string;
+	};
+}
+
+export { sendTransaction };
