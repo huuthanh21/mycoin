596f08da9932db55bef8e09f8295b00b34807ae6
20120582 <newacc2312@gmail.com>
2024-07-14 01:58:13 +0700

    fix: Update API endpoint in constants.ts file

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,4 @@
-const API_ENDPOINT = process.env.API_ENDPOINT || "http://localhost:3030";
+const API_ENDPOINT =
+	process.env.NEXT_PUBLIC_API_ENDPOINT || "http://localhost:3030";
 
 export { API_ENDPOINT };
